af901e144a723363f6bd73ccd8daec5231f88557
Manuel Alcázar <manuzz3112@gmail.com>
2022-08-02 19:41:15 +0200

    Estructura spa y primeros datos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './core/components/header/header.component';
+import { HomeComponent } from './pages/home/home.component';
+import { StudiesComponent } from './pages/studies/studies.component';
+import { ExperienceComponent } from './pages/experience/experience.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent,
+    StudiesComponent,
+    ExperienceComponent
   ],
   imports: [
     BrowserModule,
